bc91f5c4fc7e9ba38020c200227b4ebe7044072f
slsybp <slsybp@gmail.com>
2018-01-23 15:55:01 +0800

    modify

--- Algorithm_Sort/quickSort_sb.js
+++ Algorithm_Sort/quickSort_sb.js
@@ -1,8 +1,16 @@
-// Quick Sort 
+// 快速排序
 
+var dataModule = require("./testData.js");
+
+/*！
+ * @author ssb
+ * @brief 快速排序 从小到大的顺序
+ * @param numArray 待排序数组
+ * @return 排序结果数组
+ */
 function quickSort(numbersArray) {
-    console.log("quickSort ");
+    
+
 }
 
-quickSort([23,23,45]);
 
