62e4fb8e681455dcf1b0fcc77ad2fa7e7c380d87
slsybp <slsybp@gmail.com>
2018-01-26 18:06:01 +0800

    quickSort

--- Algorithm_Sort/quickSort_sb.js
+++ Algorithm_Sort/quickSort_sb.js
@@ -8,9 +8,37 @@ var dataModule = require("./testData.js");
  * @param numArray 待排序数组
  * @return 排序结果数组
  */
-function quickSort(numbersArray) {
-    
 
+function  division (numbersArray,left,right) {
+  var pivot = numbersArray[left];
+
+  while (left < right) {
+
+    while (left < right && numbersArray[right] > pivot) {
+      right--;
+    }
+    numbersArray[left] = numbersArray[right];
+    while (left < right && numbersArray[left] < pivot) {
+      left++;
+    }
+    numbersArray[right] = numbersArray[left];
+  }
+  numbersArray[left] = pivot;
+  return left;
 }
+
+function quickSort(numbersArray,left,right) {
+  if (left < right) {
+    var midIndex = division(numbersArray,left,right);
+    quickSort(numbersArray,left,midIndex - 1);
+    quickSort(numbersArray,midIndex + 1,right);
+  }
+}
+
+console.log("原始数据：" + dataModule.numbers);
+numArray = dataModule.numbers;
+
+let quickSortResult = quickSort(numArray,0,numArray.length -1);
+console.log(numArray);
 
 
